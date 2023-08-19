8bfc129abca89fd030e1008fd5699616c118b8e0
salikbubu <salikbula@gmail.com>
2023-08-19 21:38:48 +0200

    ...

--- client/src/Components/Navbar/Navigation.jsx
+++ client/src/Components/Navbar/Navigation.jsx
@@ -1,28 +1,29 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import MenuItem from "./MenuItem";
 
 const Navigation = () => {
+  const navigate = useNavigate();
   const location = useLocation();
-
-  const handleShop = () => {
-    window.location.href = "/shop";
-  };
-
-  const handlemen = () => {
-    window.location.href = "/men";
-  };
-
-  const handlewomen = () => {
-    window.location.href = "/women";
-  };
 
   return (
     <nav>
       <ul className="flex flex-row items-center gap-1">
-        <MenuItem label="Shop" onClick={handleShop} isActive={location.pathname === '/shop'} />
-        <MenuItem label="Men" onClick={handlemen} isActive={location.pathname === '/men'} />
-        <MenuItem label="Women" onClick={handlewomen} isActive={location.pathname === '/women'} />
+        <MenuItem
+          label="Shop"
+          onClick={() => navigate("/shop")}
+          isActive={location.pathname === "/shop"}
+        />
+        <MenuItem
+          label="Men"
+          onClick={() => navigate("/men")}
+          isActive={location.pathname === "/men"}
+        />
+        <MenuItem
+          label="Women"
+          onClick={() => navigate("/women")}
+          isActive={location.pathname === "/women"}
+        />
       </ul>
     </nav>
   );
